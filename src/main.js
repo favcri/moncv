dfe9aa1d0656c2ae8ffba0742e48be2e28e41f8d
Genier Tanguy <tanguy.genier@he-arc.ch>
2020-06-24 13:11:30 +0200

    pour le 1er deploy

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import $ from"jquery";
+import $ from "jquery";
 window.$ = $;
-import"bootstrap";
-import"bootswatch/dist/darkly/bootstrap.min.css";
-import"@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap";
+import "bootswatch/dist/darkly/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 $(document).ready(() => {
-    console.log("it works!");
-  });
+  console.log("it works!");
+});
