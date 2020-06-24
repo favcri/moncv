2463ae7ef9ec94e9928428d9ba4bc6104741d98c
Genier Tanguy <tanguy.genier@he-arc.ch>
2020-06-24 18:37:03 +0200

    ajout smoot scroll

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ window.$ = $;
 import "bootstrap";
 import "bootswatch/dist/darkly/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "jquery-smooth-scroll";
 
+$(document).ready(() => {
+    $("a").smoothScroll();
+});
 $(document).ready(() => {
   console.log("it works!");
 });
