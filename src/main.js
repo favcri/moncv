b0243bd143d75bb99687d116b89498349ad85440
Genier Tanguy <tanguy.genier@he-arc.ch>
2020-06-25 08:13:05 +0200

    Nouvelle structure avec un main.css Index.html presque à 0...

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import $ from "jquery";
 window.$ = $;
 import "bootstrap";
-import "bootswatch/dist/darkly/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootswatch/dist/darkly/bootstrap.min.css";
+import "./main.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "jquery-smooth-scroll";
 
-$(document).ready(() => {
-    $("a").smoothScroll();
-});
 $(document).ready(() => {
   console.log("it works!");
 });
