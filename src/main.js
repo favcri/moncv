dc59333ec98eb65057c8a86de7062d278540d5ff
Genier Tanguy <tanguy.genier@he-arc.ch>
2020-06-25 13:04:13 +0200

    Ajout des donuts avec du JS. Mise en commentaire de la 1ere partie de l'index

--- src/main.js
+++ src/main.js
@@ -5,7 +5,44 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // import "bootswatch/dist/darkly/bootstrap.min.css";
 import "./main.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import Chart from "chart.js";
 
 $(document).ready(() => {
-  console.log("it works!");
+  const progressBars = findProgressBars();
+  replaceProgressBars(progressBars);
+  $(".carousel").carousel({
+    interval: 30000
+  });
 });
+
+function findProgressBars() {
+  return [...document.querySelectorAll(".progress")];
+}
+
+function replaceProgressBars(progressBars) {
+  for (let progressBar of progressBars) {
+    const valueElement = progressBar.querySelector(".progress-bar");
+    var value = valueElement.getAttribute("aria-valuenow");
+    // var color =
+    var canvas = document.createElement("canvas");
+    progressBar.replaceWith(canvas);
+
+    createChart(canvas, value);
+  }
+}
+
+function createChart(ctx, value) {
+  new Chart(ctx, {
+    type: "doughnut",
+    data: {
+      datasets: [
+        {
+          data: [value, 100 - value],
+          backgroundColor: ["", "white"],
+          borderColor: "white"
+        }
+      ]
+    },
+    options: {}
+  });
+}
