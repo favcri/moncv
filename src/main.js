6498acb86e3eb0436e040a35e2165f99513eae25
Genier Tanguy <tanguy.genier@he-arc.ch>
2020-06-27 13:19:08 +0200

    ouais on essaie des trucs là...

---
+++ src/main.js
@@ -0,0 +1,68 @@
+import $ from "jquery";
+window.$ = $;
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootswatch/dist/darkly/bootstrap.min.css";
+import "./main.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "jquery-smooth-scroll";
+import Chart from "chart.js";
+import Darkmode from "darkmode-js";
+
+new Darkmode().showWidget();
+var options = {
+  time: "0.5s", // default: "0.3s"
+  mixColor: "#fff", // default: "#fff"
+  backgroundColor: "#fff", // default: "#fff"
+  buttonColorDark: "#100f2c", // default: "#100f2c"
+  buttonColorLight: "#fff" // default: "#fff"
+};
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
+$(document).ready(() => {
+  $("a").smoothScroll();
+  const progressBars = findProgressBars();
+  replaceProgressBars(progressBars);
+  $(".carousel").carousel({
+    interval: 30000
+  });
+});
+
+function findProgressBars() {
+  return [...document.querySelectorAll(".progress")];
+}
+
+function replaceProgressBars(progressBars) {
+  for (let progressBar of progressBars) {
+    const valueElement = progressBar.querySelector(".progress-bar");
+    var value = valueElement.getAttribute("aria-valuenow");
+    // var color =
+    var canvas = document.createElement("canvas");
+    // var desc =
+    progressBar.replaceWith(canvas);
+
+    canvas.maintainAspectRatio = false;
+    canvas.parentNode.style.height = "100%";
+    canvas.parentNode.style.width = "50%";
+
+    createChart(canvas, value);
+  }
+}
+
+function createChart(ctx, value) {
+  new Chart(ctx, {
+    type: "doughnut",
+    data: {
+      datasets: [
+        {
+          data: [value, 100 - value],
+          backgroundColor: ["#212529", "white"],
+          borderColor: "white"
+        }
+      ]
+    },
+    options: {}
+  });
+}
